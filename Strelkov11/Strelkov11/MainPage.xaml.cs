d8bf902cebaad873a242643e3f88822e9899d464
Mikckey <mihstrell888@bk.ru>
2022-05-06 02:08:06 +0300

    Добавьте файлы проекта.

---
+++ Strelkov11/Strelkov11/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Strelkov11
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SetData();
+        }
+
+        void SetData()
+        {
+            var rnd = new Random();
+            var data = new[] { "Группа: ПКС-31", "ФИО: Стрелков Михаил Алексеевич", $"Сегодня:{DateTime.Now}" };
+            foreach (var text in data)
+            {
+                var tempLabel = new Label() { HorizontalTextAlignment = TextAlignment.Center, TextColor = Color.FromRgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255)), Text = text };
+                var tempLabel1 = new Label() { HorizontalTextAlignment = TextAlignment.Center, TextColor = Color.FromRgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255)), Text = text };
+            }
+        }
+    }
+}
